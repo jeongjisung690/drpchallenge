59593c226591d15c7048bff5a19035111e530139
Jiseong <90659573+jeongjisung690@users.noreply.github.com>
2025-04-24 02:29:10 +0900

    Update policy.py

--- policy/policy.py
+++ policy/policy.py
@@ -19,31 +19,15 @@ def policy(obs, env):
     if runner is None:
         runner = PolicyRunner(
             model_path=get_model_path(env),
-            #model_path="agent.th",
             input_shape=len(obs[0]),
             n_actions=env.n_actions,
             agent_num=env.agent_num
         )
-
     
     actions = []
     for agi in range(env.agent_num):
         _, avail_actions = env.get_avail_agent_actions(agi, env.n_actions)
         action = runner.get_action(agi, obs[agi], avail_actions)
         actions.append(action)
-
-    # safety control
-    for i in range(env.agent_num):
-        if env.current_goal[i] != None:
-            continue
-        for j in range(env.agent_num):
-            if i == j:
-                continue
-            if actions[i] == actions[j]:
-                actions[i] = env.current_start[i]
-                break
-            elif actions[i] == env.current_start[j] and actions[j] == env.current_start[i]:
-                actions[i] = env.current_start[i]
-                break    
 
     return actions
